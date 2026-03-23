7e006a1211efd02c3730b621eefd846f3d03877a
Pedro Pontes García <pp457@cornell.edu>
2026-03-23 11:19:56 -0400

    Added special case for rare image-based puzzles

--- src/web.rs
+++ src/web.rs
@@ -17,17 +17,20 @@ pub struct Category {
 
 #[derive(Deserialize)]
 pub struct Card {
+    #[serde(default)]
     pub content: String,
+    pub image_alt_text: String,
 }
 
 pub fn request_web() -> Connections {
-    let date_str: chrono::format::DelayedFormat<chrono::format::StrftimeItems<'_>> =
+    let date_str =
         Local::now().format("%Y-%m-%d");
-    let response: ApiResponse =
-        reqwest::blocking::get(format!("https://www.nytimes.com/svc/connections/v2/{date_str}.json"))
-            .expect("Could not fetch content from NY Times.")
-            .json()
-            .expect("Could not parse fetched content from NY Times.");
+    let response: ApiResponse = reqwest::blocking::get(format!(
+        "https://www.nytimes.com/svc/connections/v2/{date_str}.json"
+    ))
+    .expect("Could not fetch content from NY Times.")
+    .json()
+    .expect("Could not parse fetched content from NY Times.");
     Connections {
         connections: response
             .categories
@@ -35,10 +38,21 @@ pub fn request_web() -> Connections {
             .enumerate()
             .map(|(i, c)| Connection {
                 color: ConnectionColor::from_id(i),
-                words: c.cards.iter().map(|c| c.content.clone()).collect(),
+                words: c
+                    .cards
+                    .iter()
+                    .map(|c| {
+                        if c.content.len() > 0 {
+                            c.content.clone()
+                        } else {
+                            c.image_alt_text.clone()
+                        }
+                    })
+                    .collect(),
                 hint: c.title.clone(),
                 solved: false,
-            }).collect(),
+            })
+            .collect(),
         solve_order: vec![],
         seed: 0,
     }
